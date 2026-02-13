4e0f98cb62babf93c3a0c4ec96b2d17caa0b5b62
Pierre Romera <hello@pirhoo.com>
2026-02-13 22:06:34 +0100

    refactor: remove width/height params from tooltipPlugin

--- docs/.vitepress/plugins/tooltipPlugin.js
+++ docs/.vitepress/plugins/tooltipPlugin.js
@@ -4,21 +4,20 @@ import { Tooltip } from '../components/charts/Tooltip.js';
  * Factory returning a tooltip plugin object.
  *
  * @param {Object} options
- * @param {d3.Selection} options.parent  - The SVG group to append the tooltip to
- * @param {number}       [options.width] - Container width for edge-aware positioning
- * @param {number}       [options.height]- Container height for edge-aware positioning
+ * @param {d3.Selection} options.parent  - The SVG group for coordinate conversion
  * @param {Function}     options.bind    - Called on every postDraw: bind(chart, tooltip, data)
  */
-function tooltipPlugin({ parent, width, height, bind }) {
+function tooltipPlugin({ parent, bind }) {
   return {
     name: 'tooltip',
     install(chart) {
-      chart.tooltip = new Tooltip(parent, { width, height });
+      chart.tooltip = new Tooltip(parent);
     },
     postDraw(chart, data) {
       bind(chart, chart.tooltip, data);
     },
     destroy(chart) {
+      chart.tooltip?.destroy();
       chart.tooltip = null;
     },
   };
