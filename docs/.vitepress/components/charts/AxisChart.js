3f95eb38d512df5d82077df479bdd0fdafd50242
Pierre Romera <hello@pirhoo.com>
2026-02-15 14:27:15 +0000

    feat: enhance AxisChart with position, format, and grid options

--- docs/.vitepress/components/charts/AxisChart.js
+++ docs/.vitepress/components/charts/AxisChart.js
@@ -1,4 +1,4 @@
-import { axisBottom, axisLeft } from 'd3-axis';
+import { axisBottom, axisTop, axisLeft, axisRight } from 'd3-axis';
 import 'd3-transition';
 import { D3Blueprint } from 'd3-blueprint';
 
@@ -12,6 +12,11 @@ class AxisChart extends D3Blueprint {
     this.configDefine('xTickCount', { defaultValue: undefined });
     this.configDefine('yTickCount', { defaultValue: undefined });
     this.configDefine('gridAxis', { defaultValue: 'y' });
+    this.configDefine('xAxis', { defaultValue: 'bottom' });
+    this.configDefine('yAxis', { defaultValue: 'left' });
+    this.configDefine('xTickFormat', { defaultValue: null });
+    this.configDefine('yTickFormat', { defaultValue: null });
+    this.configDefine('showDomain', { defaultValue: false });
 
     this.xAxisGroup = this.base.append('g').attr('class', 'x-axis');
     this.yAxisGroup = this.base.append('g').attr('class', 'y-axis');
@@ -29,29 +34,56 @@ class AxisChart extends D3Blueprint {
     const xTickCount = this.config('xTickCount');
     const yTickCount = this.config('yTickCount');
     const gridAxis = this.config('gridAxis');
+    const xAxisPos = this.config('xAxis');
+    const yAxisPos = this.config('yAxis');
+    const xTickFormat = this.config('xTickFormat');
+    const yTickFormat = this.config('yTickFormat');
+    const showDomain = this.config('showDomain');
     this._firstDraw = false;
 
-    const xAxis = axisBottom(xScale)
-      .tickSize(gridAxis === 'x' ? -innerHeight : 0)
-      .tickPadding(10);
-    if (xTickCount != null) xAxis.ticks(xTickCount);
+    // X axis
+    if (xAxisPos !== 'none') {
+      const xAxisFn = xAxisPos === 'top' ? axisTop(xScale) : axisBottom(xScale);
+      xAxisFn
+        .tickSize(gridAxis === 'x' || gridAxis === 'both' ? -innerHeight : 0)
+        .tickPadding(10);
+      if (xTickCount != null) xAxisFn.ticks(xTickCount);
+      if (xTickFormat) xAxisFn.tickFormat(xTickFormat);
 
-    this.xAxisGroup
-      .attr('transform', `translate(0,${innerHeight})`)
-      .transition()
-      .duration(duration)
-      .call(xAxis);
+      const xTranslateY = xAxisPos === 'top' ? 0 : innerHeight;
+      this.xAxisGroup
+        .attr('transform', `translate(0,${xTranslateY})`)
+        .transition()
+        .duration(duration)
+        .call(xAxisFn);
 
-    this.xAxisGroup.select('.domain').remove();
+      if (!showDomain) this.xAxisGroup.select('.domain').remove();
+      this.xAxisGroup.style('display', null);
+    } else {
+      this.xAxisGroup.style('display', 'none');
+    }
 
-    const yAxis = axisLeft(yScale)
-      .tickSize(gridAxis === 'y' ? -innerWidth : 0)
-      .tickPadding(10);
-    if (yTickCount != null) yAxis.ticks(yTickCount);
+    // Y axis
+    if (yAxisPos !== 'none') {
+      const yAxisFn = yAxisPos === 'right' ? axisRight(yScale) : axisLeft(yScale);
+      yAxisFn
+        .tickSize(gridAxis === 'y' || gridAxis === 'both' ? -innerWidth : 0)
+        .tickPadding(10);
+      if (yTickCount != null) yAxisFn.ticks(yTickCount);
+      if (yTickFormat) yAxisFn.tickFormat(yTickFormat);
 
-    this.yAxisGroup.transition().duration(duration).call(yAxis);
+      const yTranslateX = yAxisPos === 'right' ? innerWidth : 0;
+      this.yAxisGroup
+        .attr('transform', yTranslateX ? `translate(${yTranslateX},0)` : null)
+        .transition()
+        .duration(duration)
+        .call(yAxisFn);
 
-    this.yAxisGroup.select('.domain').remove();
+      if (!showDomain) this.yAxisGroup.select('.domain').remove();
+      this.yAxisGroup.style('display', null);
+    } else {
+      this.yAxisGroup.style('display', 'none');
+    }
   }
 }
 
